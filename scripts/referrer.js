45974076d85615bf63666ce3681e06e8653e6424
donghyuc-kim <msoopul@gimdonghyeog-ui-MacBook-Pro.local>
2016-02-22 21:32:02 +0900

    index.html의 기준이 아니라 donghyuc.com 도메인 기준으로 변경

--- scripts/referrer.js
+++ scripts/referrer.js
@@ -1,13 +1,6 @@
 
 var widgetNavigatorIndexElem;
-switch(document.referrer) {
-	case "http://mujinote.donghyuc.com/index.html": 
-	case "http://mujinote.donghyuc.com/": 
-		break; 
-	default: 
-	
-		widgetNavigatorIndexElem = document.getElementById('widget-navigator--index'); 
-		widgetNavigatorIndexElem.className = '_includes';
-
-		break; 
+if(document.referrer.indexOf('donghyuc.com') < 0) {
+	widgetNavigatorIndexElem = document.getElementById('widget-navigator--index'); 
+	widgetNavigatorIndexElem.className = '_includes';	
 }
